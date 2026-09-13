05b695a5f9d0c370d07311dfb07d76d4ab5b74e5
Machinee <shaulmbshaul01@gmail.com>
2026-09-13 09:50:13 +0100

    Update Code Lab

---
+++ Python/hello.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print(5 + 5)
+
+name = "Kelvin" #str = text (must be in quotes)
+age = 24 #int = whole number
+gpa = 3.5 #float = decimal number
+is_student = True #bool = true or false
+
+print("My name is " + name)
+print("I am " + str(age) + " years old")
+print("My GPA is " + str(gpa))
+print("I am a student: " + str(is_student)) 
